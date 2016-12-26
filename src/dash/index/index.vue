18d8009eae375da0350d651b56a8870b8d4cce65
rahul <ro87630872@126.com>
2016-12-26 11:33:03 +0800

    【UPD】更新项目目录

---
+++ src/dash/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <el-radio  v-model="radio1" label="禁用">备选项</el-radio>
+    <el-radio  v-model="radio1" label="选中且禁用">备选项</el-radio>
+    <el-checkbox v-model="checked1" disabled>备选项1</el-checkbox>
+    <el-checkbox v-model="checked2" disabled>备选项</el-checkbox>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {
+        radio1: '选中且禁用'
+      }
+    },
+    created: function () {
+    }
+  }
+</script>
+
--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import hello from '../pages/hello/hello'
 import dash from '../pages/dash/dash';
 import login from '../pages/login/login';
+import index from '../dash/index/index';
 
 export default [
   {
@@ -10,7 +11,13 @@ export default [
       {
         path: 'hello',
         name: 'hello',
-        component: hello},
+        component: hello
+      },
+      {
+        path: 'index',
+        name: 'index',
+        component: index
+      },
     ]
   },
   {
