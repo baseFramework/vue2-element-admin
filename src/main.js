1915fa7351a288b7a3ebbd258a30e1038c443e0c
wurh <ro87630872@126.com>
2018-05-31 22:47:36 +0800

    【UPD】更新代码版本  依赖包版本

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import VueRouter from 'vue-router'
 import store from './store'
-import {sync} from 'vuex-router-sync'
+import { sync } from 'vuex-router-sync'
 import routerMap from './router/router'
 import '../lib/font-awesome-4.7.0/css/font-awesome.min.css'
 
 Vue.use(VueRouter);
 Vue.use(ElementUI);
 
-const router = new VueRouter({routes: routerMap})
+const router = new VueRouter({ routes: routerMap })
 sync(store, router)
 
 const app = new Vue({
-  router,
-  store
+    router,
+    store
 }).$mount('#app');
